6260c4b57b6e72b0da95641413a7c19d67b83261
XinanQ <164927144+XinanQ@users.noreply.github.com>
2024-07-11 23:29:24 +0800

    Create UserServiceImpl.java

---
+++ UserServiceImpl.java
@@ -0,0 +1,45 @@
+package service;
+
+
+import dao.UserDao;
+import model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+  
+    private UserDao userDao;
+
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+
+    @Override
+    public void addUser(User user) {
+        userDao.insertUser(user);
+    }
+
+    @Override
+    public void removeUser(Long id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    public void modifyUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userDao.selectUserById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.selectAllUsers();
+    }
+}
