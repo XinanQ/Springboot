678be6bc1b2b73de74c91a4cca66a186878dc154
XinanQ <164927144+XinanQ@users.noreply.github.com>
2024-07-11 23:15:38 +0800

    Create UserController.java

---
+++ UserController.java
@@ -0,0 +1,39 @@
+import model.User;
+import service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.removeUser(id);
+    }
+
+    @PutMapping
+    public void updateUser(@RequestBody User user) {
+        userService.modifyUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
